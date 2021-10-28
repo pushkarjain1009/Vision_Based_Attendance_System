5c90983df1f2e8ea419045078407a40396033c55
pushkarjain1009 <pushkarjain1009@gmail.com>
2021-10-28 16:45:41 +0530

    2021-10-28 16:45

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from Data_Collecting.get_faces_from_camera import collectImagesFromCamera
 from Face_Embeddings.face_embedding import extract_face
 from Training.training import trainKerasModelForFaceRecognition
-from Prediction.prediction import detectFace
+#from Prediction.prediction import detectFace
 
-name = input()
+name = input("Enter Your Name: ")
 
-collectImagesFromCamera('Dataset/Train'+name)
-out_path = extract_face('Dataset/Train'+name)
+collectImagesFromCamera('Dataset/Train/'+name)
+out_path = extract_face('Dataset/Train/'+name)
 trainKerasModelForFaceRecognition(out_path)
-detectFace()
+#detectFace()
